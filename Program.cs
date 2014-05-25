ef9bd53fbc8e1a91cf065ccf3591dc717b8cb7c9
Jacob Schlitz <jschlitz@gmail.com>
2014-05-25 17:38:09 -0400

    Hooray for hessian.

--- Program.cs
+++ Program.cs
@@ -10,28 +10,61 @@ namespace LFD_SupportVectors
   class Program
   {
     static Random R = new Random();
+    const int TRIALS = 1000;
     static void Main(string[] args)
     {
+
+      //var twoD = new int[2, 3];
+      //twoD[0, 0] = 1;
+      //twoD[0, 1] = 2;
+      //twoD[0, 2] = 3;
+      //twoD[1, 0] = 4;
+      //twoD[1, 1] = 5;
+      //twoD[1, 2] = 6;
+      //var arrArr = Enumerable.Range(0, 2).Select(r => Enumerable.Range(0, 3).Select(c => 1 + r * 3 + c).ToArray()).ToArray();
+      ////arrArr != twoD
+
       //var wTarget = new[] { GetR(), GetR() };
-      var wTarget = new[] {-0.5, 0.2, 0.9 };
+      var wTarget = new[] {0, 0.2, 0.9 };
 
-      const int TRIALS = 1000;
       var classified = Generate(TRIALS, wTarget);
-      Console.WriteLine("{0} : {1}", classified.Count(t => t.Item2 > 0), classified.Count(t => t.Item2 <= 0));
+      //Console.WriteLine("{0} : {1}", classified.Count(t => t.Item2 > 0), classified.Count(t => t.Item2 <= 0));
 
-      var wPla = DoPla(new[] { 0.0, 0.0, 0.0}, classified);
-      Console.WriteLine();
+      Console.WriteLine("{0}% wrong.", GetWithPla(wTarget, classified)*100);
+      
+      Console.ReadKey(true);
+    }
 
-      Console.WriteLine("{0} - {1}", vToStr(wPla), vToStr(Norm(wPla)));
+    public double[,] GetQ(IEnumerable<Tuple<double[], double>> classified)
+    {
+      var stripped = classified.Select(t => new Tuple<double[], double>(t.Item1.Skip(1).ToArray(), t.Item2))
+        .ToArray();
+      var result = new double[stripped.Length, stripped.Length];
+      for (int r = 0; r < stripped.Length; r++)
+      {
+        for (int c = 0; c < stripped.Length; c++)
+        {
+          //TODO: do I have to multiply x2 on the diagonal?
+          result[r, c] = stripped[r].Item2 * stripped[c].Item2 * Dot(stripped[r].Item1, stripped[c].Item1);
+        }
+      }
+      
+      return result;
+    }
+
+    private static double GetWithPla(double[] wTarget, Tuple<double[], double>[] classified)
+    {
+      var wPla = DoPla(new[] { 0.0, 0.0, 0.0 }, classified);
+      //Console.WriteLine();
+
+      //Console.WriteLine("{0} - {1}", vToStr(wPla), vToStr(Norm(wPla)));
       wPla = Norm(wPla);
 
       //test it.
-      var testData = Generate(TRIALS*10, wTarget).ToArray();
+      var testData = Generate(TRIALS * 10, wTarget).ToArray();
       var wrong = testData.Where(t => GetY(wPla, t.Item1) != t.Item2).ToArray().Count();
-      Console.WriteLine("{0}% wrong.", 100.0 * wrong / testData.Length);
-
-
-      Console.ReadKey(true);
+      return ((double)wrong) / testData.Length;
+      //Console.WriteLine("{0}% wrong.", 100.0 * wrong / testData.Length);
     }
 
     private static string vToStr(double[] wPla)
@@ -49,7 +82,7 @@ namespace LFD_SupportVectors
     {
       var misclassified = classified.Where(t => GetY(w, t.Item1) != t.Item2).ToArray();
       //Console.WriteLine("{0} misclassified.", misclassified.Length);
-      Console.Write(".");
+      //Console.Write(".");
       if (misclassified.Length == 0)
         return w;
       else
@@ -72,7 +105,12 @@ namespace LFD_SupportVectors
 
     private static double GetY(double[] x, double[] w)
     {
-      return x.Zip(w, (xn, wn) => xn * wn).Sum() < 0 ? -1.0 : 1.0;
+      return Dot(x, w) < 0 ? -1.0 : 1.0;
+    }
+
+    private static double Dot(double[] x, double[] w)
+    {
+      return x.Zip(w, (xn, wn) => xn * wn).Sum();
     }
 
     private static double[] GetX()
